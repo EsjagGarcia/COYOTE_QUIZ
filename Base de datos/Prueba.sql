6b88c1a8ff6286358c140921a99bbe3e0f5c22b3
marioguddry <mario99cocolego@gmail.com>
2016-05-26 14:12:50 -0500

    Juego avance

--- Base de datos/Prueba.sql
+++ Base de datos/Prueba.sql
@@ -3,13 +3,12 @@ CREATE TABLE USUARIOS (
 	USUARIO_NOMBRE varchar(20) NOT NULL,
 	USUARIO_KEY int,
 	USUARIO_CONTRASENIA varchar(100),
-	COLOR varchar(7),
 	PRIMARY KEY (USUARIO_NOMBRE)
 );
-INSERT INTO USUARIOS VALUES('2','LuisEnrique','201','pepe1313','#c79810');
-INSERT INTO USUARIOS VALUES('3','Elcoordi','301','123456789','#c79810');
-INSERT INTO USUARIOS VALUES('4','ElDios','401','holaholas','#62d53c');
-INSERT INTO USUARIOS VALUES('1','Margarito','315115879','12345678910','#c79810');
+INSERT INTO USUARIOS VALUES('2','Luis Enrique','201','pepe13');
+INSERT INTO USUARIOS VALUES('3','El coordi','301','1234');
+INSERT INTO USUARIOS VALUES('4','Dios','401','hola');
+INSERT INTO USUARIOS VALUES('1','M@rgarito','315115879','12345678910');
 /* -- */
 
 CREATE TABLE ALUMNOS (
@@ -17,61 +16,65 @@ CREATE TABLE ALUMNOS (
 	USUARIO_NOMBRE varchar(20),
 	USUARIO_KEY int NOT NULL,
 	ALUMNO_GRUPO int,
+	ALUMNO_CONTRASENIA varchar(15),
 	ALUMNO_PUNTAJE int,
 	ALUMNO_PARTIDAS int,
 	PRIMARY KEY (USUARIO_KEY)
 );
-INSERT INTO ALUMNOS VALUES('Margarito Lopez','Margarito','315115879','501','3','5');
+INSERT INTO ALUMNOS VALUES('Margarito Lopez','M@rgarito','315115879','501','123456789','3','5');
 /* -- */
 
 CREATE TABLE PROFESORES (
 	USUARIO_KEY int NOT NULL,
 	USUARIO_NOMBRE varchar(20),
+	PROFESOR_CONTRASENIA varchar(15),
 	PROFESOR_ASIGNATURA varchar(15),
 	PROFESOR_PREPA int,
-	PRIMARY KEY (USUARIO_NOMBRE)
+	PRIMARY KEY (USUARIO_KEY)
 );
-INSERT INTO PROFESORES VALUES('201','LuisEnrique','historia','101');
+INSERT INTO PROFESORES VALUES('201','Luis Enrique','pepe13','historia','historia');
 /* -- */
 
 CREATE TABLE COORDINADORES (
 	USUARIO_KEY int NOT NULL,
 	USUARIO_NOMBRE varchar(20),
-	PRIMARY KEY (USUARIO_NOMBRE)
+	CORDI_CONTRASENIA varchar(15),
+	PRIMARY KEY (USUARIO_KEY)
 );
-INSERT INTO COORDINADORES VALUES('301','Elcoordi');
+INSERT INTO COORDINADORES VALUES('301','El coordi','1234');
 /* -- */
 
 CREATE TABLE ADMINS (
 	USUARIO_KEY int NOT NULL,
 	USUARIO_NOMBRE varchar(20),
-	PRIMARY KEY (USUARIO_NOMBRE)
+	ADMI_CONTRASENIA varchar(15),
+	PRIMARY KEY (USUARIO_KEY)
 );
-INSERT INTO ADMINS VALUES('401','Dios');
+INSERT INTO ADMINS VALUES('401','Dios','hola');
 /* -- */
 
-CREATE TABLE PREGUNTAS (
+CREATE TABLE MATEMATICAS (
 	MATERIA varchar(15),
-	INDICADOR int(4),
+	INDICADOR int(2),
 	PREGUNTA varchar(100),
-	RESPUESTA_CORRECTA varchar(30),
-	RESPUESTA_UNO varchar(30),
-	RESPUESTA_DOS varchar(30),
-	RESPUESTA_TRES varchar(30),
-	PRIMARY KEY (INDICADOR)
+	RESPUESTA_correcta varchar(30),
+	RESPUESTA_uno varchar(30),
+	RESPUESTA_dos varchar(30),
+	RESPUESTA_tres varchar(30),
+	PRIMARY KEY (RESPUESTA_correcta)
 );
-INSERT INTO PREGUNTAS VALUES('matematicas',1,'¿Cuál de los siguentes es un conjunto vacío?','{}','vacío','{f}','{{}}');
-INSERT INTO PREGUNTAS VALUES('matematicas',2,'¿Cual es el nombre de la "chicharronera"?','Fromula general para la factorizacion de un trinomio de segundo grado',
+INSERT INTO MATEMATICAS VALUES('matematicas',1,'¿Cuál de los siguentes es un conjunto vacío?','{}','vacío','{f}','{{}}');
+INSERT INTO MATEMATICAS VALUES('matematicas',2,'¿Cual es el nombre de la "chicharronera"?','Fromula general para la factorizacion de un trinomio de segundo grado',
 'NO se','no existe en matematicas','Producto notable de segundo grado');
-INSERT INTO PREGUNTAS VALUES('matematicas',3,'¿Cuál de los siguetes incisos es el producto notable  de (a + b) (a – b)?','a2 – b2','a+b','a-b','2a+2b');
-INSERT INTO PREGUNTAS VALUES('matematicas',4,'¿Cuál es un numero imaginario?','imaginario','ai^2','ai','a');
-INSERT INTO PREGUNTAS VALUES('matematicas',5,'Se dice que ____ son densos en los enteros','Reales','Enteros','Imaginarios','Negativos');
-INSERT INTO PREGUNTAS VALUES('matematicas',6,'El conjunto a tine a numeros del 1 al 1323 y el conjunto b tiene las letras a y b,¿cual es su interseccion?','{}','a yb',
+INSERT INTO MATEMATICAS VALUES('matematicas',3,'¿Cuál de los siguetes incisos es el producto notable  de (a + b) (a – b)?','a2 – b2','a+b','a-b','2a+2b');
+INSERT INTO MATEMATICAS VALUES('matematicas',4,'¿Cuál es un numero imaginario?','imaginario','ai^2','ai','a');
+INSERT INTO MATEMATICAS VALUES('matematicas',5,'Se dice que ____ son densos en los enteros','Reales','Enteros','Imaginarios','Negativos');
+INSERT INTO MATEMATICAS VALUES('matematicas',6,'El conjunto a tine a numeros del 1 al 1323 y el conjunto b tiene las letras a y b,¿cual es su interseccion?','{}','a yb',
 'todos los numeros','Imaginarios');
-INSERT INTO PREGUNTAS VALUES('matematicas',7,'¿Cual es el producto de notable de (a+b)^2?','a^2+2ab+b^2','a^2+b^2','a+b+a+b','a*2+b*2');
-INSERT INTO PREGUNTAS VALUES('matematicas',8,'Valor de : 4!','24','96','16','8');
-INSERT INTO PREGUNTAS VALUES('matematicas',9,'El valor de : a^2/a','a','a^2','1','0');
-INSERT INTO PREGUNTAS VALUES('matematicas',10,'El siguente es un numero entero positivo','8','8.333','-8','01-feb');
-INSERT INTO PREGUNTAS VALUES('matematicas',11,'Un numero irracional','Pi','4','8.33313213','8.33333');
-INSERT INTO PREGUNTAS VALUES('matematicas',12,'¿Qué es un numero imaginario?','numero que piensas','una raiz negativa','un numero con un denominador mayor al divisor',
+INSERT INTO MATEMATICAS VALUES('matematicas',7,'¿Cual es el producto de notable de (a+b)^2?','a^2+2ab+b^2','a^2+b^2','a+b+a+b','a*2+b*2');
+INSERT INTO MATEMATICAS VALUES('matematicas',8,'Valor de : 4!','24','96','16','8');
+INSERT INTO MATEMATICAS VALUES('matematicas',9,'El valor de : a^2/a','a','a^2','1','0');
+INSERT INTO MATEMATICAS VALUES('matematicas',10,'El siguente es un numero entero positivo','8','8.333','-8','01-feb');
+INSERT INTO MATEMATICAS VALUES('matematicas',11,'Un numero irracional','Pi','4','8.33313213','8.33333');
+INSERT INTO MATEMATICAS VALUES('matematicas',12,'¿Qué es un numero imaginario?','numero que piensas','una raiz negativa','un numero con un denominador mayor al divisor',
 'un numero con un divisor mayor al denominador');
--- Templates/juego.php
+++ Templates/juego.php
@@ -3,6 +3,7 @@ echo '<!DOCTYPE html>
 		<head>
 			<title> COYOTE_QUIZ </title>
 		</head>
+		<meta charset="UTF-8-BOM">
 		<body>';
 		echo '<div id="contador"></div>';
 		if(!isset($_POST['materia']))
@@ -12,46 +13,67 @@ echo		'<form action="juego.php" method="POST">
 					<option> Matemáticas </option>
 				</select>
 				<input type="submit" value="Empezar"/>
-			<form/>';
+			</form>';
 		}
 		else
 		{
+echo 	'<script>';
+echo		'var segundos = 0;
+			function contar(){
+				if(segundos >= 360)
+				{
+					document.getElementById("contador").innerHTML = "se agoto el tiempo";
+				}
+				else 
+				{
+					segundos++;
+					document.getElementById("contador").innerHTML = "Tienes: " + segundos + " segundos.";
+				}
+				setInterval("contar()",1000);
+			}';
+echo 	'</script>';
+			
 			$conect = mysqli_connect("localhost","root");
 			$cat = $_POST['materia'];
 			if(mysqli_select_db($conect,"juego"))
 			{
-				$c = rand(1,12);
-				$search = mysqli_query($conect,"SELECT * FROM preguntas WHERE indicador LIKE $c;");
-				print_r($search);
-				echo "<br/>";
-				$na = mysqli_fetch_array($search);
-				print_r($na);
-				//$name = $na['nombre'];
+				$max = 12;
+				$sabe = array();
+				do
+				{
+					echo $max;
+					$si = 0;
+					$c = rand(1,12);
+					echo "<br/>";
+					foreach($sabe as $value)
+						if($value == $c)
+							$si = 1;
+						else
+							echo "WERTYUIO";
+					if($si < 1)
+					{
+						$sabe[] = $c;
+						$search = mysqli_query($conect,"SELECT * FROM matematicas WHERE indicador = $c");
+						echo "<br/>";
+						$na = mysqli_fetch_array($search);
+						$pregunta = $na['PREGUNTA'];
+						echo $pregunta."<br/>";
+						$r_1 = $na['RESPUESTA_correcta'];
+						echo $r_1."<br/>";
+						$r_2 = $na['RESPUESTA_uno'];
+						echo $r_2."<br/>";
+						$r_3 = $na['RESPUESTA_dos'];
+						echo $r_3."<br/>";
+						$r_4 = $na['RESPUESTA_tres'];
+						echo $r_4."<br/>";
+						$max--;
+					}
+				}
+				while($max > 0);
 			}
 			else
-			{
 				echo "algo no va bien";
-			}
 		}
-		
-echo 	'<body>';
-	echo '<script>';
-
-	echo '
-	alert ("funciona");
-var segundos = 0;
-function contar(){
-	if(segundos >= 30)
-	{
-		document.getElementById("contador").innerHTML = "se agoto el tiempo";
-	}
-	 else 
-	 {
-		segundos++;
-		document.getElementById("contador").innerHTML = "Tienes: " + segundos + " segundos.";
-	}
-}
-setInterval("contar()",1000);';
-echo '</script>
+echo	'</body>
 	</html>';
 ?>
