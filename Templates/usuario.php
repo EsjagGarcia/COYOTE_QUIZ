5477cd03d4eda8cf6d528afd36e18d5b7e72806d
Ramon1111 <jramoncito7-99@hotmail.com>
2016-05-25 23:40:43 -0500

    cambios dentro de usuario.php

--- Templates/usuario.php
+++ Templates/usuario.php
@@ -9,7 +9,8 @@
 		
 		<link href="../Documents/bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
 		<link rel="shortcut icon" href="../Sources/Resources/prepa 6.jpg" type="image/png"/>
-		<link href="../Style/stylemain.css"  rel="stylesheet" type="text/css">
+		<link href="../Style/usuario.less"  rel="stylesheet/less" type="text/css">
+		<script src="../Documents/less.js" type="text/javascript"></script>
 		<style>
 			#izquierda{
 				border:1px solid black;
@@ -26,7 +27,8 @@
 		<div class="container" id="cuer">
 		<?php
 		SESSION_START();
-		
+		if(isset($_POST['nom-usuario']) && isset($_POST['contra']))
+		{
 		$enlace=mysqli_connect("localhost","Ramon","escuelaenp6","proyectof");
 		htmlspecialchars($_POST['nom-usuario']);
 		htmlspecialchars($_POST['contra']);
@@ -56,6 +58,8 @@
 			$_SESSION['tipo']=$arre[0];
 			$_SESSION['usuario']=$arre[1];
 			$_SESSION['key']=$arre[2];
+			$_SESSION['color']=$arre[4];
+		}
 		}
 		if(isset($_SESSION['tipo']) && isset($_SESSION['usuario']) && isset($_SESSION['key']))
 		{
@@ -71,7 +75,7 @@
 									<span class="icon-bar"></span>
 								</button>
 								<a href="#" class="navbar-brand" id="imag-unam"><img alt="Brand" src="../Sources/Resources/esc-unam.png" height="140%"/></a>
-								<p class="navbar-text">Usuario _ Nom</p>
+								<p class="navbar-text">'.$_SESSION['usuario'].'</p>
 							</div>	
 						</div>	
 						<div class="col-lg-5 col-lg-offset-1 col-md-5 col-sm-6">
@@ -110,7 +114,7 @@
 								<button type="button" class="btn btn-primary navbar-btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span> <span class="caret"></span>
 								</button>
 								<ul class="dropdown-menu">
-									<li><a href="#">Diseño de página</a></li>
+									<li><a href="./camb_dis.php">Diseño de página</a></li>
 									<li><a href="#">Información personal</a></li>
 									<li role="separator" class="divider"></li>
 									<li><a href="./main.php"><span class="glyphicon glyphicon-off" aria-hidden="true"></span> Cerrar Sesión</a></li>
@@ -144,16 +148,8 @@
 							</div>
 					</div>';
 			}
-			
 		?>
-			
-			
-			
-			
-			
-			
 		</div>
-		
 		<div class="row">
 			<div class="col-lg-12 navbar-fixed-bottom">
 				<footer class="text-center" id="part-bottom">
@@ -165,7 +161,7 @@
 		</div>
 		<script src="../Documents/jquery.js"></script>
 		<script src="../Documents/bootstrap/js/bootstrap.min.js"></script>
-		<script type="text/javascript" src="main.js">
-		</script>
+		<script type="text/javascript" src="main.js"></script>
+		<script type="text/javascript" src="colorChange.js"></script>
 	</body>
 </html>
