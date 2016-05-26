c9b03df774698e434096d8ed6c7a3fa8c324e0ce
adrishg <adri315289837@gmail.com>
2016-05-25 23:09:48 -0400

    Add files via upload

---
+++ Templates/registro_profesor.php
@@ -0,0 +1,81 @@
+<?php
+$nombre=$_POST['nombre'];
+$usuario0$_POST['usuario'];
+$numero=$_POST['numero'];
+$asignatura=$_POST['asignatura'];
+$prepa=$_POST['prepa'];
+$contra=$_POST['contra'];
+$sena=$_POST['sena'];
+$ch=str_split($contra);
+	$contrasena="";
+	for($x=0;$x<5;$x++)
+	{
+		$wi=rand(0,9);
+		$contrasena=$contrasena.$wi;
+	}
+	$carac=0;
+	foreach($ch as $p)
+	{
+		$nu=ord($p);
+		$carac+=$nu;
+	}
+	$contrasena=$contrasena.$carac;
+	for($x=0;$x<strlen($contra);$x++)
+	{
+		$wi=(ord($ch[$x])>>1)-4;
+		$contrasena=$contrasena.chr($wi);
+	}
+	
+	$cad=array();
+	$arreglo=array();
+	$cont=strlen($contra);
+	for($i=0;$i<$cont;$i++)
+	{
+		$car=substr($contra,$i,1);
+		array_push($cad,$car);
+	}
+	$mul=ceil($cont/5);
+	$contadorpal=0;
+	for($x=0;$x<$mul;$x++)
+	{
+		$eje=array();
+		for($y=0;$y<5;$y++)
+		{
+			if($contadorpal<$cont)
+				array_push($eje,$cad[$y]);
+			else
+				array_push($eje,'');
+			$contadorpal++;
+		}
+		array_push($arreglo,$eje);
+		for($g=0;$g<5;$g++)
+			if($cad!='\0')
+				array_shift($cad);
+	}
+	$grr=array();
+	for($y=0;$y<5;$y++)
+		for($x=0;$x<$mul;$x++)
+			array_push($grr,$arreglo[$x][$y]);
+	$grr=implode("",$grr);
+	$h='Texto: '.$contra.'<br/>playfair("'.$grr.'",5)';
+	$cant=ceil(strlen($grr)/2);
+	$contrasena=$contrasena.substr($grr,0,$cant);
+
+
+if($contra==$sena)
+{
+	$link=MySQLi_connect("localhost","root","","proyectof");
+	$tildes = $link->query("SET NAMES 'utf8'");
+	$SQL="INSERT INTO USUARIOS(`USUARIOS_TYPE`,`USUARIO_NOMBRE`,`USUARIO_KEY`,`USUARIO_CONTRASENIA`)
+	values ('2','$usuario','$numero','$contrasena');";
+	$SQL2='INSERT INTO COORDINADORES (`USUARIO_NOMBRE`,`USUARIO_KEY`,`USUARIO_CONTRASENIA`) VALUES ('.$usuario.','.$numero.','.$contrasena.')';
+	MySQLi_query($link,$SQL);
+	MySQLi_query($link,$SQL2);
+	header('location:cordinador.html');
+	mysqli_close($link);
+}
+else
+{
+	header('location:cordinador.html');
+}
+?>
